5ba503898f9b0b8f3f5c096b5b1df01f88f7cc63
gkourtidis <gkourtidis@threenitas.com>
2025-07-10 21:51:33 +0300

    Initial Project Setup - Including Auth

---
+++ MarketCatalogue/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using MarketCatalogue.Authentication.Domain.Entities;
+using MarketCatalogue.Authentication.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketCatalogue.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+
+    public static void ConfigureDbContexts(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<AuthenticationDbContext>(options => options.UseSqlServer(
+            config.GetConnectionString("AuthenticationDbContext"),
+            b => b.MigrationsAssembly(typeof(AuthenticationDbContext).Assembly.FullName)));
+    }
+
+    public static void AddIdentity(this IServiceCollection services)
+    {
+        services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            .AddRoles<IdentityRole>()    
+            .AddEntityFrameworkStores<AuthenticationDbContext>();
+    }
+}
