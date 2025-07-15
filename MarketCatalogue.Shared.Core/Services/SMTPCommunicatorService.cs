28da51cb8d6cc5b91c0ed2b9338ce5cd17642f24
gkourtidis <gkourtidis@threenitas.com>
2025-07-15 17:50:34 +0300

    Fixed email template pathing in SMTP Communicator Service

--- MarketCatalogue.Shared.Core/Services/SMTPCommunicatorService.cs
+++ MarketCatalogue.Shared.Core/Services/SMTPCommunicatorService.cs
@@ -1,4 +1,6 @@
 ﻿using MarketCatalogue.Shared.Domain.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +12,18 @@ namespace MarketCatalogue.Shared.Application.Services;
 public class SMTPCommunicatorService : ISMTPCommunicatorService
 {
     private readonly ISMTPClientService _smtpClientService;
-
-    public SMTPCommunicatorService(ISMTPClientService smtpClientService)
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    public SMTPCommunicatorService(ISMTPClientService smtpClientService, IWebHostEnvironment webHostEnvironment)
     {
         _smtpClientService = smtpClientService;
+        _webHostEnvironment = webHostEnvironment;
     }
 
     public async Task<bool> SendEmailConfirmationEmail(string subject, string email, string message, string confirmationUrl)
     {
         var body = await RenderTemplate("ConfirmEmail.html");
-
+        if (body.IsNullOrEmpty())
+            return false;
         body = body.Replace("!URL!", confirmationUrl);
         body = body.Replace("!MESSAGE!", message);
         return await _smtpClientService.SendAsync(subject, body, [email]);
@@ -28,6 +32,8 @@ public class SMTPCommunicatorService : ISMTPCommunicatorService
     public async Task<bool> SendPurchaseConfirmationEmail(string subject, string email, string message)
     {
         var body = await RenderTemplate("PurchaseConfirmationEmail.html");
+        if (body.IsNullOrEmpty())
+            return false;
         body = body.Replace("!MESSAGE!", message);
         return await _smtpClientService.SendAsync(subject, body, [email]);
     }
@@ -35,24 +41,26 @@ public class SMTPCommunicatorService : ISMTPCommunicatorService
     public async Task<bool> SendPurchaseCancellationEmail(string subject, string email, string message)
     {
         var body = await RenderTemplate("PurchaseCancellationEmail.html");
+        if (body.IsNullOrEmpty())
+            return false;
         body = body.Replace("!MESSAGE!", message);
         return await _smtpClientService.SendAsync(subject, body, [email]);
     }
 
-    private static async Task<string> RenderTemplate(string filename)
+    private async Task<string> RenderTemplate(string filename)
     {
         try
         {
-            var templatePath = Path.Combine(@"D:\banlo\VisualStudioProjects\source\repos\MarketCatalogue\MarketCatalogue\wwwroot\emails\", filename);
+            var templatePath = Path.Combine(_webHostEnvironment.WebRootPath, "emails", filename);
 
             if (File.Exists(templatePath))
                 return await File.ReadAllTextAsync(templatePath);
             else
                 throw new FileNotFoundException($"Template file not found: {templatePath}");
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            throw new Exception($"Cannot read the email template: {ex.Message}");
+            return string.Empty;
         }
     }
 }
